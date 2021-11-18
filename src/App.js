e587339d47f0dc20441e60413c346149af5d0516
marzeu <marzahell@hotmail.com>
2021-11-18 19:38:59 -0300

    renomer pasta

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+state = { 
+  nome: 'Union'
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>Nome: </label>
+      <input type="text" value={this.state.nome} />
     </div>
   );
 }
